2c615b572291a191d4c45d526a8612d6b767d5d5
Arlecuno <palchinskij19@gmail.com>
2021-01-30 22:53:28 +0200

    implementing JWT

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const config = require('config');
 
 const User = require('../../model/User');
 
 router.post('/', async (req, res) => {
    console.log(req.body);
    const {email, password} = req.body;
-   res.send('From user1');
 
    try{ 
       let user = await User.findOne({email});
+      console.log(user);
       if(user) { 
-         res.status(400).json({err: 'User already exists'});
+         return res.status(400).json({err: [{msg: 'User already exists'}]});
       }
 
       user = new User({
@@ -25,8 +27,24 @@ router.post('/', async (req, res) => {
       await user.save();
       console.log('User registered');
 
+      const payload = {
+         user: { 
+            id: user.id
+         }
+      };
+
+      jwt.sign(
+         payload,
+         config.get('jwtSecret'),
+         {expiresIn: 360000},
+         (err, token) => { 
+            if(err) throw err;
+            res.json({token});
+         }
+         );
    } catch(err) { 
       console.log(err);
+      res.status(500).send('Server error');
    }
 });
 
