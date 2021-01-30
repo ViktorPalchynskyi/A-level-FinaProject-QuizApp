44ca477a24f170e82ceb838e5c1a00af95c03bb4
Arlecuno <palchinskij19@gmail.com>
2021-01-30 22:09:52 +0200

    add user registration with bycrypt password

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-router.get('/', (req, res) => res.send('From user'));
+const User = require('../../model/User');
+
+router.post('/', async (req, res) => {
+   console.log(req.body);
+   const {email, password} = req.body;
+   res.send('From user1');
+
+   try{ 
+      let user = await User.findOne({email});
+      if(user) { 
+         res.status(400).json({err: 'User already exists'});
+      }
+
+      user = new User({
+         email, password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      console.log('User registered');
+
+   } catch(err) { 
+      console.log(err);
+   }
+});
 
 module.exports = router;
